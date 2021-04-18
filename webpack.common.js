ef4e95163ab8aaa082fb04bf7128a82678f109ff
Marcin Czaja <marcinczaja@outlook.com>
2021-04-18 07:42:19 +0100

    old files added

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/3.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "3.html",
+    }),
   ],
 };
